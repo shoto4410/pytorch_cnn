438eaca5f5526b869b2aa940907d8892eea1181d
岸田星道 <kishiri@arayanoMacBook-Pro.local>
2022-07-07 12:09:30 +0900

    create class FlattenLayer and net

--- main.py
+++ main.py
@@ -12,3 +12,24 @@ batch_size = 128
 train_loader = DataLoader(fashion_mnist_train, batch_size=batch_size, shuffle=True)
 test_loader = DataLoader(fashion_mnist_test, batch_size=batch_size, shuffle=True)
 
+#(N,C,H,W)形式のTensorを(N, C*H*W)に引伸ばす層
+#畳み込み層の出力をMLPに渡す際に必要
+class FlattenLayer(nn.Module):
+    def forward(self, x):
+        sizes = x.size()
+        return x.view(sizes[0], -1)
+    
+#2dは画像形式用
+conv_net = nn.Sequential(
+    nn.Conv2d(1, 32, 5),
+    nn.MaxPool2d(2),
+    nn.ReLU(),
+    nn.BatchNorm2d(32),
+    nn.Dropout2d(0.25),
+    nn.Conv2d(32, 64, 5),
+    nn.MaxPool2d(2),
+    nn.ReLU(),
+    nn.BatchNorm2d(64),
+    nn.Dropout2d(0.25),
+    FlattenLayer()
+)
